f23e5cbd4342907b0f76cb473a01ec3091c59cde
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-03-14 18:02:07 +0700

    improvement: update config

--- src/layouts/Main/index.jsx
+++ src/layouts/Main/index.jsx
@@ -1,9 +1,5 @@
 const MainLayout = ({ children }) => {
-  return (
-    <section className="flex justify-center items-center w-full h-full">
-      {children}
-    </section>
-  );
+  return <section className="flex justify-center items-center w-full h-full">{children}</section>;
 };
 
 export default MainLayout;
